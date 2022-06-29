5a3f44fe4a8c51024e235551f216155980aff952
Sandra Milena Quintero Leal <smquinterol@uqvirtual.edu.co>
2022-06-28 19:31:13 -0500

    Se crea el src

---
+++ src/main/java/RepasoJava.java
@@ -0,0 +1,27 @@
+public class RepasoJava {
+    public static void main (String[] args){
+        //ejercicio 1
+        int numCats = 5;
+        double gradeExam = 13.2;
+        char letter = 'a';
+        String message = "Hola mundo 123";
+        boolean isFree = false;
+
+        // ejercicio 2
+        /*
+        Este es un ejercicio de comentarios
+         */
+
+        // ejercicio 3
+        String bloodPressureString = "15.2";
+        String numDogsString = "5";
+        int numPencils= 3;
+        double moneyOnBank = 52.3;
+
+        double bloodPressure = Double.parseDouble(bloodPressureString);
+
+
+
+
+    }
+}
---
+++ src/main/java/pruebassimples/PruebasSimples.java
@@ -0,0 +1,4 @@
+package pruebassimples;
+
+public class PruebasSimples {
+}
